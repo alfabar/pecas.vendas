211e47dc778edd206a1448cabbf144299e3ba456
alfabar <alfa_bar@hotmail.com>
2022-04-23 17:04:47 -0300

    atualizado

--- main/urls.py
+++ main/urls.py
@@ -4,10 +4,10 @@ from . import views
 from django.conf import settings
 from django.conf.urls.static import static
 
-urlpatterns=[
+urlpatterns=[ 
     path('',views.home,name='home'),
     path('search',views.search,name='search'),
-    path('category-list',views.category_list,name='category-list'),
+    path('lista-categoria',views.lista_categoria,name='lista-categoria'),
     path('brand-list',views.brand_list,name='brand-list'),
     path('product-list',views.product_list,name='product-list'),
     path('category-product-list/<int:cat_id>',views.category_product_list,name='category-product-list'),
