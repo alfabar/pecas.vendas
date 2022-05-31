0486863f19d853a3224bf5e2b5eb83131359bac9
Adriano Baram <alfa_bar@hotmail.com>
2022-05-31 18:20:01 -0300

    make it better

--- lojaPecas/settings.py
+++ lojaPecas/settings.py
@@ -188,8 +188,8 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
 
-MEDIA_URL = 'media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'static')
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 LOGIN_REDIRECT_URL='home'
 LOGOUT_REDIRECT_URL='login'
