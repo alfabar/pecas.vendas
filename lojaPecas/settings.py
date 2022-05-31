7142a459cfc55cd79add4585907a018e75a52f69
Adriano Baram <alfa_bar@hotmail.com>
2022-05-31 17:48:16 -0300

    make it better

--- lojaPecas/settings.py
+++ lojaPecas/settings.py
@@ -180,7 +180,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = "/var/www/frango-mais.herokuapp.com/static/"
 STATIC_URL = 'static/'
 
 # Extra places for collectstatic to find static files.
