f786b2b25f2ebcaa097c0c2fea91cd9f6ab21e7f
Adriano Baram <alfa_bar@hotmail.com>
2022-06-02 10:37:29 -0300

    atualizado

--- main/template_context.py
+++ main/template_context.py
@@ -3,14 +3,14 @@ from django.db.models import Min,Max
 def get_filters(request):
 	cats=Produto.objects.distinct().values('categoria__titulo','categoria__id')
 	marcas=Produto.objects.distinct().values('marca__titulo','marca__id')
-	colors=ProdutoAtributo.objects.distinct().values('color__title','color__id','color__color_code')
-	sizes=ProdutoAtributo.objects.distinct().values('size__title','size__id')
-	minMaxPrice=ProdutoAtributo.objects.aggregate(Min('price'),Max('price'))
+	cor=ProdutoAtributo.objects.distinct().values('cor__titulo','cor__id','cor__cor_code')
+	tamanho=ProdutoAtributo.objects.distinct().values('tamanho__titulo','tamanho__id')
+	minMaxPreco=ProdutoAtributo.objects.aggregate(Min('preco'),Max('preco'))
 	data={
 		'cats':cats,
 		'marcas':marcas,
-		'colors':colors,
-		'sizes':sizes,
-		'minMaxPrice':minMaxPrice,
+		'cor':cor,
+		'tamanho':tamanho,
+		'minMaxPrice':minMaxPreco,
 	}
 	return data
